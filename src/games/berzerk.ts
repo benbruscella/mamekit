fab93a8985d9a8a0c7103c7bab9391af59bac136
Ben Bruscella <benbruscella@gmail.com>
2026-08-20 16:48:00 +1000

    berzerk

--- src/games/berzerk.ts
+++ src/games/berzerk.ts
@@ -14,15 +14,18 @@ export const berzerk = sourceTarget({
     { atFrame: 850, code: 'Space', heldFrames: 30, releasedFrames: 20 },
   ],
   golden: {
-    regions: { maincpu: '648be64f', speech: 'f63d0fde' },
+    regions: {
+      maincpu: '648be64f',
+      speech: 'f63d0fde',
+    },
     checkpoints: {
-      '1': { video: 'f7e6ac81', state: '553557d0' },
-      '60': { video: 'f7e6ac81', state: '0a5c68dd' },
-      '180': { video: 'f7e6ac81', state: '20c1b0d8' },
-      '300': { video: 'f7e6ac81', state: '0c5b2708' },
-      '600': { video: '8090479d', state: 'a5841e6f' },
-      '900': { video: 'aa9820ea', state: '4e1b9c0b' },
-      '1200': { video: 'e3563cb2', state: 'f28aac30' },
+      1: { video: 'f7e6ac81', state: '553557d0' },
+      60: { video: 'f7e6ac81', state: '0a5c68dd' },
+      180: { video: 'f7e6ac81', state: '20c1b0d8' },
+      300: { video: 'f7e6ac81', state: '0c5b2708' },
+      600: { video: '8090479d', state: 'a5841e6f' },
+      900: { video: 'aa9820ea', state: '4e1b9c0b' },
+      1200: { video: '6cc91e34', state: 'f28aac30' },
     },
     audio: {
       writes: 70224,
